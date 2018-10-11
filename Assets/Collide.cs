ff7fa260eea389aa36c648df1166ec77f86901cc
LP2851 <lucas.phillips28@hotmail.com>
2018-10-11 10:51:33 +0100

    Added all work done so far
    Everything works but gets laggy quickly

---
+++ Assets/Collide.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Sun") && other.transform.localScale.x < 10)
+        {
+            other.transform.localScale += this.transform.localScale;
+            //this.gameObject.SetActive(false);
+            Destroy(this);
+            
+        }
+        else if (other.gameObject.CompareTag("Sphere"))
+        {
+            if(other.transform.localScale.x > this.transform.localScale.x && other.transform.localScale.x < 4)
+            {
+                other.transform.localScale += other.transform.localScale;
+                //this.gameObject.SetActive(false);
+                Destroy(this);
+            }
+            else
+            {
+                this.transform.localScale += other.transform.localScale;
+                //other.gameObject.SetActive(false);
+                Destroy(other);
+            }
+            
+            
+        }
+        if(this.transform.localScale.x > 20)
+        {
+            Destroy(this);
+        }
+    }
+
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject obj;
+    public GameObject obj2;
+    public GameObject obj3;
+    public List<GameObject> objlist;
+    
+    // Use this for initialization
+    void Start () {
+        objlist.Add(obj);
+        objlist.Add(obj2);
+        objlist.Add(obj3);
+
+        for(int i = 0; i != 20; i++)
+        {
+            GameObject test = Instantiate(obj);
+            test.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            objlist.Add(test);
+
+        }
+        InvokeRepeating("Spawn", 0.1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Spawn()
+    {
+        for (int i = 0; i != objlist.Count; i++)
+        {
+            float x = Random.Range(5f, 10f);
+            float y = Random.Range(5f, 10f);
+            float z = Random.Range(5f, 10f);
+            float val = Random.Range(1f, 5f);
+            objlist[i].transform.localScale = new Vector3(val, val, val);
+            Instantiate(objlist[i], objlist[i].transform.TransformVector(x, y, z), Quaternion.identity);
+        }
+    }
+}
